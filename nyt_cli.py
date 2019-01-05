2e80319e42b72adbcb2d0764162f778950b578b1
Uma Desai <uma.desai@students.olin.edu>
2019-01-04 20:14:22 -0800

    first pass

---
+++ nyt_cli.py
@@ -0,0 +1,44 @@
+"""
+Python command-line interface with click and the New York Times API.
+"""
+import os
+import click
+import requests
+
+api_key = os.getenv("API_KEY")
+
+
+def top_story(section, api_key):
+    url = f'https://api.nytimes.com/svc/topstories/v2/{section}.json'
+
+    query_params = {
+        'apikey': api_key
+    }
+
+    response = requests.get(url, params=query_params)
+    return response.json()['results'][0]['title']
+
+
+@click.command()
+@click.argument('section')
+@click.option(
+    '--api-key', '-a',
+    envvar="API_KEY",
+    help='your API key for the New York Times API',
+)
+def main(section, api_key):
+    """
+    A news tool that shows you the top NYT story in the SECTION of
+    your choice. Allowed SECTION values are: home, opinion, world,
+    national, politics, upshot, nyregion, business, technology, science,
+    health, sports, arts, and books.
+
+    You need a valid API key from the New York Times for the tool to work.
+    You can sign up for a free account at https://developer.nytimes.com/signup.
+    """
+    story = top_story(section, api_key)
+    print(f"The top story in {section} right now: {story}.")
+
+
+if __name__ == "__main__":
+    main()
